20d97e05595dc25b7ea3498cad04f497839a5aa3
navi42chd-ai <navi42chd@gmail.com>
2026-07-03 11:45:12 +0530

    Update subjects.js

--- js/subjects.js
+++ js/subjects.js
@@ -24,54 +24,7 @@ const SUBJECTS = [
         file: "data/ancient-history/ch03-vedic-age.js",
         dataVar: "chapter_vedic_age",
       },
-      {
-        id: "ch04",
-        label: "Ch 4: Buddhism and Jainism",
-        file: "data/ancient-history/ch04-buddhism-jainism.js",
-        dataVar: "chapter_buddhism_jainism",
-      },
-      {
-        id: "ch05",
-        label: "Ch 5: Magadha Empire",
-        file: "data/ancient-history/ch05-magadha.js",
-        dataVar: "chapter_magadha",
-      },
-      {
-        id: "ch06",
-        label: "Ch 6: Mauryan Empire",
-        file: "data/ancient-history/ch06-mauryan-empire.js",
-        dataVar: "chapter_mauryan",
-      },
-      {
-        id: "ch07",
-        label: "Ch 7: Central Asian Contact",
-        file: "data/ancient-history/ch07-central-asian.js",
-        dataVar: "chapter_central_asian",
-      },
-      {
-        id: "ch08",
-        label: "Ch 8: The Satavahanas",
-        file: "data/ancient-history/ch08-satavahanas.js",
-        dataVar: "chapter_satavahanas",
-      },
-      {
-        id: "ch09",
-        label: "Ch 9: The Gupta Empire",
-        file: "data/ancient-history/ch09-gupta-empire.js",
-        dataVar: "chapter_gupta",
-      },
-      {
-        id: "ch10",
-        label: "Ch 10: Harshavardhana",
-        file: "data/ancient-history/ch10-harshavardhana.js",
-        dataVar: "chapter_harsha",
-      },
-      {
-        id: "ch11",
-        label: "Ch 11: Kingdoms of South",
-        file: "data/ancient-history/ch11-south-kingdoms.js",
-        dataVar: "chapter_south_kingdoms",
-      },
+      
     ],
   },
 ];
